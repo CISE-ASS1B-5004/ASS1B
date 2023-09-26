a96d9c40df8d3288edb34ea714bcac4d336a9268
Zack <zakku.lim@gmail.com>
2023-09-26 15:46:31 +1300

    finished home navbar css
    added search and login items.
    moved title to the top and made it big.
    add a nice hovering effect

--- frontend/src/components/PopulatedNavBar.tsx
+++ frontend/src/components/PopulatedNavBar.tsx
@@ -1,17 +1,21 @@
 import { IoMdArrowDropdown } from "react-icons/io";
 import NavBar from "./nav/NavBar";
 import NavItem from "./nav/NavItem";
+import styles from "./PopulatedNavBar.module.scss";
 
 const PopulatedNavBar = () => {
   return (
-    <div>
-      <div>SPEED</div> {/* Moved SPEED above the NavBar */}
+    <div className={styles.container}>
+      <div className={styles.title}>SPEED</div> {/* Moved SPEED above the NavBar */}
+      <div className={styles.nav}>
       <NavBar>
         <NavItem route="/">Home</NavItem>
         <NavItem route="/articles">View Articles</NavItem>
         <NavItem route="/articles/new">Submit New</NavItem>
+        <NavItem route="/articles/search">Search</NavItem>
         <NavItem end route="/login">Login</NavItem>
       </NavBar>
+      </div>
     </div>
   );
 };
