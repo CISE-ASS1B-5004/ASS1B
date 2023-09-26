3d554138e601521357121a5d36d770997b5b1aef
Zack <zakku.lim@gmail.com>
2023-09-27 11:44:01 +1300

    created login page
    - a popup window
    - change the navbar to display which user is current
    - successful login directs to placeholder page.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import PopulatedNavBar from "../components/PopulatedNavBar";
+import { UserRoleProvider } from "../pages/UserContext"
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <PopulatedNavBar />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <UserRoleProvider>
+      <SessionProvider session={session}>
+        <PopulatedNavBar />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </UserRoleProvider>
   );
 }
 export default MyApp;
