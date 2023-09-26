2ea42d9d9c3b0ff131ed38183400fe56fe75b4a3
Eirik Enriquez <eirikdbbd@gmail.com>
2023-09-26 14:06:57 +1300

    added confirmation message

--- frontend/src/components/SubmissionForm.tsx
+++ frontend/src/components/SubmissionForm.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import formStyles from "../styles/Form.module.scss";
 import axios from "axios";
 
 export default function SubmissionForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const onSubmit = async (data: any) => {
     const url = "http://localhost:8082/api/articles";
@@ -13,100 +14,113 @@ export default function SubmissionForm() {
       const realAuthors = data.authors.split(",");
       data.authors = realAuthors;
 
-      const response = await axios.post(url, data, {
+      await axios.post(url, data, {
         headers: {
           "Content-Type": "application/json",
         },
       });
 
-      console.log("Response:", response.data);
+      console.log("Submitted successfully!");
+      setIsSubmitted(true);
+      reset(); // Reset the form fields
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
+  const confirmationMessage = isSubmitted && (
+    <div className={formStyles.confirmation}>
+      Submission successful! Thank you.
+    </div>
+  );
+
   return (
-    <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
-      <h1>Submit a new article</h1>
+    <div>
+      {confirmationMessage}
+      <form
+        className={formStyles.form}
+        onSubmit={handleSubmit(onSubmit)}
+      ></form>
+      <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input className={formStyles.input}
+            {...register("title")}
+            type="text"
+            id="title"
+            placeholder="Title"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="title">Title:</label>
-        <input className={formStyles.input}
-          {...register("title")}
-          type="text"
-          id="title"
-          placeholder="Title"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="authors">Authors (comma-separated):</label>
+          <input className={formStyles.input}
+            {...register("authors")}
+            type="text"
+            id="authors"
+            placeholder="Authors"
+            required
+          />
+        </div>
 
-      <div >
-        <label htmlFor="authors">Authors (comma-separated):</label>
-        <input className={formStyles.input}
-          {...register("authors")}
-          type="text"
-          id="authors"
-          placeholder="Authors"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="journalName">Journal Name:</label>
+          <input className={formStyles.input}
+            {...register("journalName")}
+            type="text"
+            id="journalName"
+            placeholder="Journal Name"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="journalName">Journal Name:</label>
-        <input className={formStyles.input}
-          {...register("journalName")}
-          type="text"
-          id="journalName"
-          placeholder="Journal Name"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="pubYear">Publication Year:</label>
+          <input className={formStyles.input}
+            {...register("pubYear")}
+            type="number"
+            id="pubYear"
+            placeholder="Publication Year"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="pubYear">Publication Year:</label>
-        <input className={formStyles.input}
-          {...register("pubYear")}
-          type="number"
-          id="pubYear"
-          placeholder="Publication Year"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="volume">Volume:</label>
+          <input className={formStyles.input}
+            {...register("volume")}
+            type="number"
+            id="volume"
+            placeholder="Volume"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="volume">Volume:</label>
-        <input className={formStyles.input}
-          {...register("volume")}
-          type="number"
-          id="volume"
-          placeholder="Volume"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="pages">Pages:</label>
+          <input className={formStyles.input}
+            {...register("pages")}
+            type="number"
+            id="pages"
+            placeholder="Pages"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="pages">Pages:</label>
-        <input className={formStyles.input}
-          {...register("pages")}
-          type="number"
-          id="pages"
-          placeholder="Pages"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="doi">DOI:</label>
+          <input className={formStyles.input}
+            {...register("doi")}
+            type="text"
+            id="doi"
+            placeholder="DOI"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="doi">DOI:</label>
-        <input className={formStyles.input}
-          {...register("doi")}
-          type="text"
-          id="doi"
-          placeholder="DOI"
-          required
-        />
-      </div>
-
-      <input type="submit" className={formStyles.buttonItem}/>
-    </form>
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
