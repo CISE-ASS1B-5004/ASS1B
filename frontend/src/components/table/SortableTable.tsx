c70d4e8e715f365a00ba9cf87e58e373df5922ed
Eirik Enriquez <eirikdbbd@gmail.com>
2023-10-16 18:03:15 +1300

    added sorting to table

--- frontend/src/components/table/SortableTable.tsx
+++ frontend/src/components/table/SortableTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import tableStyles from "./SortableTable.module.scss";
 
 interface SortableTableProps {
@@ -6,25 +6,74 @@ interface SortableTableProps {
   data: any[];
 }
 
-const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => (
-  <table className={tableStyles.table}>
-    <thead>
-      <tr className={tableStyles.headings}>
-        {headers.map((header) => (
-          <th className={tableStyles.th} key={header.key}>{header.label}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((row, i) => (
-        <tr key={i}>
+const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => {
+  const [sortColumn, setSortColumn] = useState(""); // Current sorting column
+  const [sortDirection, setSortDirection] = useState("asc"); // Sorting direction: "asc" or "desc"
+
+  const toggleSort = (columnKey: string) => {
+    if (columnKey === sortColumn) {
+      // If the same column is clicked again, reverse the sorting direction
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      // If a different column is clicked, set it as the new sorting column with "asc" direction
+      setSortColumn(columnKey);
+      setSortDirection("asc");
+    }
+  };
+
+  const sortedData = data.slice().sort((a, b) => {
+    const aValue = a[sortColumn];
+    const bValue = b[sortColumn];
+
+    if (sortDirection === "asc") {
+      if (aValue < bValue) {
+        return -1;
+      } else if (aValue > bValue) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else {
+      if (bValue < aValue) {
+        return -1;
+      } else if (bValue > aValue) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
+  return (
+    <table className={tableStyles.table}>
+      <thead>
+        <tr className={tableStyles.headings}>
           {headers.map((header) => (
-            <td className={tableStyles.td} key={header.key}>{row[header.key]}</td>
+            <th
+              className={tableStyles.th}
+              key={header.key}
+              onClick={() => toggleSort(header.key)}
+              style={{ cursor: "pointer" }}
+            >
+              {header.label}{" "}
+              {header.key === sortColumn && sortDirection === "asc" ? "↑" : "↓"}
+            </th>
           ))}
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {sortedData.map((row, i) => (
+          <tr key={i}>
+            {headers.map((header) => (
+              <td className={tableStyles.td} key={header.key}>
+                {row[header.key]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default SortableTable;
